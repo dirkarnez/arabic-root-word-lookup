591a1f19d114d859f8754e94b6260af5f55c3923
Dirk Arnez <smalldirkalex@gmail.com>
2025-03-03 22:50:06 +0800

    - update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const a = "كتب";
-a.length
+class RegExp1 extends RegExp {
+  [Symbol.match](str) {
+    const result = RegExp.prototype[Symbol.match].call(this, str);
+    if (result) {
+      return "VALID";
+    }
+    return "INVALID";
+  }
+}
+const a = "ك";
+const b = "ت";
+const c = "ب";
+console.log("كَتَبَ".match(new RegExp1(`${a}.*${b}.*${c}`)));
